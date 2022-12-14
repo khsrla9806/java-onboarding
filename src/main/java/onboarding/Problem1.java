505fd0282c1bfd79f8f1a44507c999da5673857c
khsrla9806 <kimhunsope@naver.com>
2022-12-14 16:34:13 +0900

    feat: 페이지 각 자리수 곱을 구하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -7,6 +7,18 @@ class Problem1 {
         int sumOfDigit = (page / 100) + (page % 100 / 10) + (page % 10);
         return sumOfDigit;
     }
+
+    public static int multiplyEachDigitOfPage(int page) {
+        int lengthOfPageValue = String.valueOf(page).length();
+        if (lengthOfPageValue == 3) {
+            return (page / 100) * (page % 100 / 10) * (page % 10);
+        }
+        if (lengthOfPageValue == 2) {
+            return (page / 10) * (page % 10);
+        }
+        return page;
+    }
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
