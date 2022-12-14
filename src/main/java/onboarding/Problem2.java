65cf8126bcfbe9408f621d10b45e1851221ef904
khsrla9806 <kimhunsope@naver.com>
2022-12-14 18:03:55 +0900

    feat(PROBLEM2): 입력된 문자열을 리스트로 변환하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,18 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
         return answer;
+    }
+
+    public static List<String> cryptogramToList(String cryptogram) {
+        return Arrays.stream(cryptogram.split(""))
+                .collect(Collectors.toList());
     }
 }
