b6c6304e6097c849885fe5d3bb61f2a0147d6e36
khsrla9806 <kimhunsope@naver.com>
2022-12-14 16:06:35 +0900

    feat(PROBLEM1): 각 자리수 합을 구하는 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,6 +3,10 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    public static int addEachDigitOfPage(int page) {
+        int sumOfDigit = (page / 100) + (page % 100 / 10) + (page % 10);
+        return sumOfDigit;
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
