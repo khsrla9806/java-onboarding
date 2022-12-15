11711f50d83e6e36ef0e642cebff6f066317d928
khsrla9806 <kimhunsope@naver.com>
2022-12-15 22:10:31 +0900

    feat(PROBLEM5): 문제 5번 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return countEachNumberOfMoney(money);
+    }
+
+    public static List<Integer> countEachNumberOfMoney(int money) {
+        int[] kindOfMoney = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> result = new ArrayList<>();
+        for (int value : kindOfMoney) {
+            result.add(money / value);
+            money %= value;
+        }
+        return result;
     }
 }
