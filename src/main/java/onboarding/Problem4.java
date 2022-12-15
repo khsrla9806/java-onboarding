84921432b19fcd0b9cc8384b38d2a3a0b3bdd61e
khsrla9806 <kimhunsope@naver.com>
2022-12-15 22:00:52 +0900

    feat(PROBLEM4): 4번 문제 완료

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,26 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        Arrays.stream(word.split(""))
+                .forEach((value) -> answer.append(convertText(value)));
+
+        return answer.toString();
+    }
+
+    public static String convertText(String text) {
+        if (Character.isUpperCase(text.charAt(0))) {
+            char convertedText = (char)((int)'A' + (int)'Z' - (int)text.charAt(0));
+            return String.valueOf(convertedText);
+        }
+
+        if (Character.isLowerCase(text.charAt(0))) {
+            char convertedText = (char)((int)'a' + (int)'z' - (int)text.charAt(0));
+            return String.valueOf(convertedText);
+        }
+        return text;
     }
 }
