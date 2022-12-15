a949a89e853e3519a3babd1ede8aaf2743052fe4
khsrla9806 <kimhunsope@naver.com>
2022-12-15 21:27:21 +0900

    fear(PROBLEM3): 문제 3번 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,22 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return countNumberOfClap(number);
+    }
+
+    public static int countNumberOfClap(int number) {
+        int count = 0;
+        List<Integer> numbersToCheck = List.of(3, 6, 9);
+        for (int i = 1; i <= number; i++) {
+            long temp = Arrays.stream(String.valueOf(i).split(""))
+                    .filter((digit) -> numbersToCheck.contains(Integer.valueOf(digit)))
+                    .count();
+            count += temp;
+        }
+        return count;
     }
 }
